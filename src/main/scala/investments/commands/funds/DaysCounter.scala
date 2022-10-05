b078c9acb837fc515e39dfa95ac021431466cb14
Eduardo R. D'Avila <erdavila@gmail.com>
2022-10-05 19:13:01 -0300

    funds/FundsMain: ensure presence of last day of month

--- src/main/scala/investments/commands/funds/DaysCounter.scala
+++ src/main/scala/investments/commands/funds/DaysCounter.scala
@@ -1,20 +1,27 @@
 package sisgrana
 package investments.commands.funds
 
-import java.time.{DayOfWeek, LocalDate}
+import java.time.{DayOfWeek, LocalDate, YearMonth}
 import scala.math.Ordering.Implicits._
 
 class DaysCounter(noPriceDates: Set[LocalDate]) {
-  def count(from: LocalDate, to: LocalDate): Int = {
-    def isWeekendDate(date: LocalDate): Boolean =
-      date.getDayOfWeek match {
-        case DayOfWeek.SATURDAY | DayOfWeek.SUNDAY => true
-        case _ => false
-      }
-
+  def count(from: LocalDate, to: LocalDate): Int =
     Iterator.iterate(from)(_.plusDays(1))
       .drop(1)
       .takeWhile(_ <= to)
-      .count(date => !noPriceDates.contains(date) && !isWeekendDate(date))
-  }
+      .count(!isNoPriceDate(_))
+
+  def lastDateOfYearMonth(yearMonth: YearMonth): LocalDate =
+    Iterator.iterate(yearMonth.atEndOfMonth())(_.minusDays(1))
+      .find(!isNoPriceDate(_))
+      .get
+
+  def isNoPriceDate(date: LocalDate): Boolean =
+    noPriceDates.contains(date) || isWeekendDate(date)
+
+  private def isWeekendDate(date: LocalDate): Boolean =
+    date.getDayOfWeek match {
+      case DayOfWeek.SATURDAY | DayOfWeek.SUNDAY => true
+      case _ => false
+    }
 }
--- src/main/scala/investments/commands/funds/FundsMain.scala
+++ src/main/scala/investments/commands/funds/FundsMain.scala
@@ -1,22 +1,33 @@
 package sisgrana
 package investments.commands.funds
 
+import com.softwaremill.quicklens._
 import investments.fileTypes.fundsMonthStatement._
 import java.time.YearMonth
 
 object FundsMain {
-  // TODO: warn when there is no data for the last day of the month
   // TODO: when listing multiple months, check if final recordSet from previous month matches initialRecordSet of current month
 
   def main(args: Array[String]): Unit = {
     val yearMonth = YearMonth.parse(args(0))
     val statement = FundsMonthStatementFileReader.read(yearMonth)
-    val (initialRecordSet, recordSets) = StatementProcessor.process(yearMonth, statement)
+    val completeStatement = ensureLastDayOfMonth(statement, yearMonth)
+
+    val (initialRecordSet, recordSets) = StatementProcessor.process(yearMonth, completeStatement)
 
     val printer = new Printer(
       accumulated = args.contains("--accumulated"),
       totalsOnly = args.contains("--totals-only"),
     )
     printer.printMonthRecordSets(yearMonth, initialRecordSet, recordSets)
+  }
+
+  private def ensureLastDayOfMonth(statement: FundsStatement, yearMonth: YearMonth): FundsStatement = {
+    val daysCounter = new DaysCounter(statement.noPriceDates)
+    val lastDate = daysCounter.lastDateOfYearMonth(yearMonth)
+
+    statement
+      .modify(_.entries)
+      .usingIf(!statement.entries.contains(lastDate))(_ + (lastDate -> Map.empty))
   }
 }
